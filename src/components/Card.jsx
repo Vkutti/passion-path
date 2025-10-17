5fbd3a29c0a9e4afd07a5e42525dbce799e33689
Venkat Kutti <venkat.k.kutti@gmail.com>
2025-10-17 13:38:03 -0700

    new spacing added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../css/Card.css'
 import Modal from './Modal'
+import { supabase } from '../lib/supabase'
 
 function Card({ title, desc }) {
 
@@ -9,13 +10,21 @@ function Card({ title, desc }) {
     const openModal = () => setShowModal(true)
     const closeModal = () => setShowModal(false)
 
+    const getSession = async () => {
+        const { data: { session }, error } = await supabase.auth.getSession();
+        return { session, error };
+    }
+
+
+
+
     return (
         <>
             <div className='card-holder' onClick={openModal}>
                 <h1 className='card-text'>{ title }</h1>
             </div>
 
-            {showModal && (<Modal close={closeModal} titleVar={title} descVar={desc} />)}
+            {showModal && (<Modal save={console.log("hi")} close={closeModal} titleVar={title} descVar={desc} />)}
         </>
     )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,13 @@
 import '../css/Modal.css'
 
-function Modal({close, titleVar, descVar}) {
+function Modal({save, close, titleVar, descVar}) {
     return (
         <div className='modal-background' onClick={close}>
             <div className='modal-card-bg'>
                 <h1 className='title-text'>{titleVar}</h1>
                 <h2 className='desc-text'>{descVar}</h2>
                 <div className='modal-btn-holder'>
+                    <button onClick={save} className='primary-btn'>Save</button>
                     <button onClick={close} className='tertiary-btn'>Close</button>
                 </div>
             </div>
