c8abca3078269e5ebd297503bd80312b8ccc6d4f
Venkat Kutti <venkat.k.kutti@gmail.com>
2025-10-18 14:27:16 -0700

    saving works

--- src/pages/SavedItems.jsx
+++ src/pages/SavedItems.jsx
@@ -3,11 +3,24 @@ import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 import '../css/Card.css'
 import Modal from "../components/Modal";
+import { supabase } from '../lib/supabase'
+import { useState } from "react";
+import { useEffect } from "react";
 
 function SavedItems() {
+    const [saved, setSaved] = useState([])
+    
+    useEffect(() => {getSaved()}, [])    
 
+    async function getSaved() {
+        const { data: { session } } = await supabase.auth.getSession();
+        const user = session?.user;
 
-
+        const { data } = await supabase.from("user_saved").select().eq('user', user.email)
+        setSaved(data)
+        console.log(saved)
+    }
+    
     return (
         <div>
             <Navbar />
@@ -19,8 +32,22 @@ function SavedItems() {
             </div>
 
             <div className="cards-holder">
-                <Card title="Saved" desc="Its been saved"/>
-                
+                {saved.map((items, index) => (
+                    Array.isArray(items.info) ? (
+                        items.info.map((item, idx) => (
+                            <div key={index + '-' + idx} >
+                                
+                                <Card title={item.title} desc={item.description}/>
+                            </div>
+                        ))
+                    ) : (
+                        <div key={index}>
+                            
+                            <Card title={item.info.title} desc={item.info.description}/>
+
+                        </div>
+                    )
+                ))}
             </div>
         </div>
     )
